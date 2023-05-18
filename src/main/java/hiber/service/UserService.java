358d55d373f45aaf6f2c9bf9cd2fb9982614e70f
Rustem <grimhuman1@gmail.com>
2023-05-18 23:35:36 +0300

    pp_2.2.1

--- src/main/java/hiber/service/UserService.java
+++ src/main/java/hiber/service/UserService.java
@@ -1,10 +1,13 @@
 package hiber.service;
 
 import hiber.model.User;
+import org.springframework.ui.Model;
 
 import java.util.List;
 
 public interface UserService {
     void add(User user);
     List<User> listUsers();
+
+    User getUserByCar(String model, int series);
 }
